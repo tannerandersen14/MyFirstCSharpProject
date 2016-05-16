3a9092a847b5999bc6a4c2aee1e5629bed053051
Tanner Andersen <tanner.andersen14@gmail.com>
2016-05-16 14:40:02 -0600

    Few working APIs using IDictionary.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        // GET: api/Customer
+        [HttpGet]
+        [Route("api/customer")]
+        public IEnumerable<Customer> Get()
+        {
+            Customer customerTemp = new Customer();
+            List<Customer> customers = new List<Customer>();
+            foreach (Customer customer in Customer.Customers.Values) {
+                customers.Add(customer);
+            }
+            return customers.AsEnumerable();
+        }
+
+        // GET: api/Customer/5
+        [HttpGet]
+        [Route("api/customer/{customerId}")]
+        public Customer Get(int customerId)
+        {
+            return Customer.Customers[customerId];
+        }
+
+        // POST: api/Customer
+        [HttpPost]
+        [Route("api/customer")]
+        public IHttpActionResult Post([FromBody]Customer customer)
+        {
+            Customer.Customers.Add(customer.CustomerId, customer);
+            return this.Created<Customer>("/api/customer/" + customer.CustomerId.ToString(), customer);
+        }
+
+        // PUT: api/Customer/5
+        [HttpPut]
+        [Route("api/customer/{customerId}")]
+        public HttpResponseMessage Put(int customerId, [FromBody]Customer customer)
+        {
+            HttpResponseMessage result = null;
+            if (customerId != customer.CustomerId)
+                result = this.Request.CreateResponse(HttpStatusCode.ExpectationFailed, "Don't try to hack...you could end up in jail...");
+            else
+            {
+                Customer.Customers[customerId] = customer;
+                result = this.Request.CreateResponse(HttpStatusCode.OK, customer);
+            }
+            return result;
+        }
+
+        // DELETE: api/Customer/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Customer
+    {
+        public static IDictionary<int, Customer> Customers { get; set; }
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Customer(){ if (Customers == null) {
+                Customers = new Dictionary<int, Customer>();
+                Customers.Add(1, new Customer { CustomerId = 1, Email = "yes@yes.com", Name = "Yes Sir" });
+                Customers.Add(2, new Customer { CustomerId = 2, Email = "hello@hello.com", Name = "Hello Hello" });
+            }
+        }    
+    }
+}
